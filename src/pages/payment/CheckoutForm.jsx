c9e0e5f1c096d4340e31492db87f198b68dc6248
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-06-21 03:57:34 +0600

    payment done, post payment form starts

--- src/pages/payment/CheckoutForm.jsx
+++ src/pages/payment/CheckoutForm.jsx
@@ -1,9 +1,28 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-
-const CheckoutForm = () => {
+import { useEffect, useState } from "react";
+import useAxiosSecure from '../../hooks/useAxiosSecure'
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+const CheckoutForm = ({ scholarshipData }) => {
+    const [error, setError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const { user } = useAuth();
+    const [transactionId, setTransactionId] = useState('');
 
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+
+    const { _id, applicationfee } = scholarshipData;
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price: applicationfee })
+            .then(res => {
+                console.log(res.data.clientSecret);
+                setClientSecret(res.data.clientSecret);
+            })
+    }, [axiosSecure, applicationfee])
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -18,45 +37,86 @@ const CheckoutForm = () => {
             return;
         }
 
-        const {error,paymentMethod}= await stripe.createPaymentMethod({
-            type:'card',
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
             card
         })
 
-        if(error){
-            console.log('In CheckOut form ::payment error',error)
-        }else{
-            console.log('In CheckOut form ::payment error',paymentMethod);
+        if (error) {
+            console.log('In CheckOut form ::payment error', error)
+            setError(error.message);
+        } else {
+            console.log('In CheckOut form ::payment error', paymentMethod);
+            setError('');
         }
+
+
+        //confirm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email,
+                    name: user?.displayName
+                }
+            }
+        })
+
+        if (confirmError) {
+            console.log(confirmError)
+            setError(confirmError.message)
+            setTransactionId('');
+        } else {
+            setError('');
+            console.log('payment intent', paymentIntent);
+            if (paymentIntent.status === 'succeeded') {
+                console.log('transaction id: ', paymentIntent.id)
+                setTransactionId(paymentIntent.id)
+                Swal.fire({
+                    icon: "success",
+                    title: "Payment is Successful",
+                    text: `Transaction Id: ${transactionId}`,
+                });
+            }
+        }
+
+
 
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '18px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <div className="mb-[100px]">
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '18px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-                className="border py-3 shadow-sm shadow-yellow-200 rounded-md mb-3 px-1"
-            >
-            </CardElement>
+                    }}
+                    className="border py-3 shadow-sm shadow-yellow-200 rounded-md mb-3 px-1"
+                >
+                </CardElement>
 
-            <button className="border px-3 font-medium tracking-wider rounded-md mb-[100px] bg-yellow-200 hover:bg-yellow-300" type="submit" disabled={!stripe}>
-                Pay
-            </button>
+                <button className="border px-4 py-1 text-[18px] mb-2 font-medium tracking-wider rounded-md border-b-yellow-300 border-b-4 cursor-pointer" type="submit"
+                    disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+                <p className="tracking-wider text-red-700 font-medium">{error}</p>
+                {
+                    transactionId && <p className="tracking-wider text-green-800 font-normal mt-2 pl-1"><span className="font-bold">Transaction Id:</span> {transactionId}</p>
+                }
 
-        </form>
+            </form>
+        </div>
     );
 };
 
--- src/pages/payment/PaymentInitial.jsx
+++ src/pages/payment/PaymentInitial.jsx
@@ -1,25 +1,51 @@
 import { useParams } from 'react-router-dom';
-import HeadSubHead from '../../components/HeadSubHead'
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
+import { useEffect, useState } from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+
 const PaymentInitial = () => {
-    const id = useParams();
-    console.log(id);
+    const [scholarship, setScholarship] = useState(null);
+    const axiosSecure = useAxiosSecure();
+    const { id } = useParams();
+    console.log('in payment initial', id);
+
+    useEffect(() => {
+        const fetchScholarship = async () => {
+            const res = await axiosSecure.get(`/scholarship/payment/${id}`)
+            setScholarship(res.data);
+        }
+
+        fetchScholarship();
+    }, [axiosSecure, id])
+
+    console.log('cart', scholarship);
+
 
     const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
     console.log(stripePromise);
 
     return (
         <div>
-            <div>
-                <HeadSubHead heading1="Payment" heading2="Portal" subHeading="Proceed your payment"></HeadSubHead>
-            </div>
-            <div>
-                <Elements stripe={stripePromise}>
-                     <CheckoutForm></CheckoutForm>
-                </Elements>
-            </div>
+            {
+                scholarship ?
+                    <div>
+                        <div>
+                            <div className="mx-[5px] md:mx-[50px] mt-[20px] mb-[60px]">
+                                <h1 className="text-3xl md:text-[43px] mb-[17px] font-medium tracking-wider">Payment <span className="border-b-[3px] border-yellow-300">portal</span></h1>
+                                <h1 className="text-[22px] tracking-wider">Proceed your payment</h1>
+                            </div>
+                        </div>
+                        <div className='mx-[5px] md:mx-[50px]'>
+                            <Elements stripe={stripePromise}>
+                                <CheckoutForm scholarshipData={scholarship}></CheckoutForm>
+                            </Elements>
+                        </div>
+                    </div>
+                    :
+                    <div className="flex justify-center items-center min-h-screen"><span className="loading loading-bars text-yellow-300 w-24"></span></div>
+            }
         </div>
     );
 };
