cc0d2246a1341fcb5fa9b79b933f2f7bfe4810f0
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-06-26 02:34:17 +0600

    review top-4

--- src/components/HeadSubHead.jsx
+++ src/components/HeadSubHead.jsx
@@ -3,8 +3,8 @@ const HeadSubHead = ({ heading1, heading2, subHeading }) => {
     return (
         <div>
             <div className="mx-[5px] md:mx-[50px] mt-[100px] mb-[60px]">
-                <h1 className="text-3xl md:text-[43px] mb-[17px] font-medium tracking-wider">{heading1} <span className="border-b-[3px] border-yellow-300">{heading2}</span></h1>
-                <h1 className="text-[22px] tracking-wider">{subHeading}</h1>
+                <h1 className="text-3xl md:text-[40px] mb-[17px] font-medium tracking-wider">{heading1} <span className="border-b-[3px] border-yellow-300">{heading2}</span></h1>
+                <h1 className="text-[20px] tracking-wider">{subHeading}</h1>
             </div>
 
             
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
 import HeadSubHead from "../components/HeadSubHead";
 import Top6_Scholarship from '../components/Top6_Scholarship/Top6_Scholarship'
+import ReviewHomePage from "../components/ReviewHomePage";
 
 const Home = () => {
     return (
@@ -9,9 +10,10 @@ const Home = () => {
             <Banner></Banner>
             <HeadSubHead heading1="Top" heading2="Scholarships" subHeading="Premier Opportunities for Advanced Study and Research"></HeadSubHead>
             <Top6_Scholarship></Top6_Scholarship>
-            <div className="mx-[5px] md:mx-[50px] mb-[100px] flex justify-end">
+            <div className="mx-[5px] md:mx-[50px] flex justify-end">
                 <Link className="w-[120px] text-center text-[18px]" to='/allScholarships'><h1 className="border-b-yellow-300 border-b-4 pb-2 tracking-wider font-medium">Explore more</h1></Link>
             </div>
+            <ReviewHomePage></ReviewHomePage>
         </div>
     );
 };
